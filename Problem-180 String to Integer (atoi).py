b1a86da7a0ccbf950d1bbb8a56a43971e0239a79
Adhish Shrivastava <adhish70@gmail.com>
2022-01-22 19:36:18 -0500

    Initial Commit

---
+++ Problem-180 String to Integer (atoi).py
@@ -0,0 +1,11 @@
+# 8. String to Integer (atoi)
+# https://leetcode.com/problems/string-to-integer-atoi/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        pass
